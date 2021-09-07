04658585709689af373377f6df424b3e8bfec3de
Joalck454 <89622491+Joalck454@users.noreply.github.com>
2021-09-07 23:04:16 +1000

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>           
+</body>
+</html>
+
+<!--Just a placeholder to start using GitHub for our #sprint task, 
+This index will be a front page to explain our goals/tasks. And also direct users to different pages (e.g Add data, read data, export data etc). -->
