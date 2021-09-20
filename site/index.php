2deeb49d6801a31084e8fd244cd0b8c9984757c9
atul <atul45682@gmail.com>
2021-09-20 21:20:48 +1000

    new

--- site/index.php
+++ site/index.php
@@ -6,7 +6,7 @@
     <p>
         Hey you there! from HTML.
         <br/>
-        Jack.
+        Jack. jjk
         <?php echo "Hello from PHP ".phpversion()."!"; ?>
         <br/>
         <?php
@@ -15,12 +15,12 @@ include_once("db.php");
 if($conn) {
     echo "Database connected!";
 } else {
-    echo "Database couldn't connect.";
+    echo "Database couldn't connect .";
 }
         ?>
     </p>
 </body>
 </html>
 
-<!--Just a placeholder to start using GitHub for our #sprint task, 
+<!--Just a placeholder to start using GitHub for our #sprint task,
 This index will be a front page to explain our goals/tasks. And also direct users to different pages (e.g Add data, read data, export data etc). -->
