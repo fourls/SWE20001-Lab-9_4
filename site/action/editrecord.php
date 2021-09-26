aee00890dbf7a2df9944816282d31429e95e98b4
Elliot Hillary <103074503@student.swin.edu.au>
2021-09-26 15:53:11 +1000

    Add in existence check to edit record

--- site/action/editrecord.php
+++ site/action/editrecord.php
@@ -15,8 +15,25 @@ if(!isset($_POST["description"]))
 if(!isset($_POST["quantity"]))
     fail("Please provide a quantity.");
 
+$id = $_POST["product_id"];
+
+$id_exists_stmt = $conn->prepare("SELECT count(*) AS count FROM products WHERE product_id = ?");
+$id_exists_stmt->bind_param("i", $id);
+
+$result = $stmt->execute();
+
+if(!$result) {
+    fail("Database error.");
+} else {
+    $fetch = $result->fetch_assoc();
+
+    if($fetch["count"] == 0) {
+        fail("No product exists with that ID.");
+    }
+}
+
 $stmt = $conn->prepare("UPDATE PRODUCTS SET product_name = ?, description = ?, quantity = ? WHERE product_id = ?");
-$stmt->bind_param("ssii",$_POST["product_name"], $_POST["description"], $_POST["quantity"], $_POST["product_id"]);
+$stmt->bind_param("ssii",$_POST["product_name"], $_POST["description"], $_POST["quantity"], id);
 
 $result = $stmt->execute();
 
