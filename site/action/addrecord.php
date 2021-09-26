386ce99f6751f86765744773dca26762fa597b1f
Elliot Hillary <103074503@student.swin.edu.au>
2021-09-26 14:58:33 +1000

    Create addrecord.php

---
+++ site/action/addrecord.php
@@ -0,0 +1,28 @@
+<?php
+
+require("../db.php");
+
+function fail($msg) {
+    echo "<a href=\"add.php\">".$msg."</a>";
+    die();
+}
+
+if(!isset($_POST["product_id"]))
+    fail("Please provide a product ID.");
+if(!isset($_POST["product_name"]))
+    fail("Please provide a product name.");
+if(!isset($_POST["description"]))
+    fail("Please provide a description.");
+if(!isset($_POST["quantity"]))
+    fail("Please provide a quantity.");
+
+$stmt = $conn->prepare("INSERT INTO products (product_id, product_name, description, quantity) VALUES (?,?,?,?)");
+$stmt->bind_param("sssd",$_POST["product_id"],$_POST["product_name"], $_POST["description"], $_POST["quantity"]);
+
+$result = $stmt->execute();
+
+if(!$result) {
+    fail("Add failed - please provide valid values.");
+} else {
+    header("Location: show.php");
+}
