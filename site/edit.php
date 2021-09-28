49b1c3b47406de61543060b848afbdca3ef363ca
Elliot Hillary <103074503@student.swin.edu.au>
2021-09-28 17:27:38 +1000

    edit.php cleanup

--- site/edit.php
+++ site/edit.php
@@ -7,26 +7,26 @@
  <title>Edit Sales</title>
 </head>
 
-
 <body>
 <h1>Edit Sales </h1>
 <form action = "action/editrecord.php" method = "post" >
 	<fieldset>
 		<legend>Details</legend>
-			<p><label for="product_id">Product ID: </label> 
-				<input type="text" id="product_id" name= "product_id" placeholder="P0001" required="required"/>
-			</p>
-			<p><label for="sales_id">Sales ID: </label> 
-				<input type="text" id="sales_id" name= "sales_id" required="required" />
-			</p>
-			 			
-			<p><label for="sale_quantity">Sales Quantity: </label> 
-				<input type="text" id="sale_quantity" name= "sale_quantity" required="required" />
-			</p>			
-</fieldset>
+		<p><label for="product_id">Product ID: </label> 
+			<input type="text" id="product_id" name= "product_id" placeholder="0001" required="required"/>
+		</p>
+		<p><label for="sales_id">Sales ID: </label> 
+			<input type="text" id="sales_id" name= "sales_id" required="required" />
+		</p>
+					
+		<p><label for="sale_quantity">Sales Quantity: </label> 
+			<input type="number" id="sale_quantity" name= "sale_quantity" required="required" />
+		</p>			
+	</fieldset>
 
-<input type = "submit" name = "posted" value = "Post">
-<input type = "reset" value = "Reset">
+	<input type = "submit" name = "posted" value = "Post">
+	<input type = "reset" value = "Reset">
+</form>
 <br>
 <br>
 <?php
