1ed711dff4cb4104245220ab1167b657de9eb9aa
Elliot Hillary <103074503@student.swin.edu.au>
2021-09-28 23:48:06 +1000

    Remove sale ID from add.php, update addrecord.php

--- site/action/addrecord.php
+++ site/action/addrecord.php
@@ -11,9 +11,11 @@ if(!isset($_POST["product_id"]))
     fail("Please provide a product ID.");
 if(!isset($_POST["sale_quantity"]))
     fail("Please provide a quantity sold.");
+if(!isset($_POST["sale_date"]))
+    fail("Please provide a sale date in Y/m/d format.");
 
-$stmt = $conn->prepare("INSERT INTO sales_record (product_id, sale_quantity, sale_date) VALUES (?,?,NOW())");
-$stmt->bind_param("ii",$_POST["product_id"], $_POST["sale_quantity"]);
+$stmt = $conn->prepare("INSERT INTO sales_record (product_id, sale_quantity, sale_date) VALUES (?,?,?)");
+$stmt->bind_param("iis",$_POST["product_id"], $_POST["sale_quantity"], $_POST["sale_date"]);
 
 $result = $stmt->execute();
 
