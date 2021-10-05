b0510428fb6bca558a1c5cffe3f727bc3669fa8d
Elliot Hillary <103074503@student.swin.edu.au>
2021-10-05 16:24:57 +1100

    Add product name

--- site/displaymonthly.php
+++ site/displaymonthly.php
@@ -4,11 +4,11 @@ require("db.php");
 $start_of_month = $_GET["date"];
 
 if(!isset($_GET["date"])) {
-    header("index.php");
+    header("Location: index.php");
     die();
 }
 
-$stmt = $conn->prepare("SELECT sale_id, product_id, sale_quantity, sale_date from sales_record WHERE sale_date BETWEEN ? AND DATE_ADD(?, INTERVAL 1 MONTH)");
+$stmt = $conn->prepare("SELECT sale_id, product_id, product_name, sale_quantity, sale_date from sales_record JOIN product WHERE sale_date BETWEEN ? AND DATE_ADD(?, INTERVAL 1 MONTH) ORDER BY sale_date ASC");
 $stmt->bind_param("ss", $start_of_month, $start_of_month);
 $stmt->execute();
 $result = $stmt->get_result();
@@ -21,32 +21,33 @@ $result = $stmt->get_result();
 </head>
 <body>
 <h1>PHP-SRePS Sales Report</h1>
-<h2>For the month beginning <?php echo date_format(date_create($start_of_month), "d/m/Y") ?></h2>
+<h2>For the month beginning <?php echo date_format(date_create($start_of_month), "d/m/Y"); ?></h2>
 <section class ="sale">
 <?php
 if (!$result) {								
     echo "<p>Something is wrong with", $query, "</p>";
-}
-else{
+} else {
     echo "<table border=\"1\">";
     echo "<tr>\n"
+            ."<th scope=\"col\">Date</th>\n"
             ."<th scope=\"col\">Sale ID</th>\n"
             ."<th scope=\"col\">Product ID</th>\n"
+            ."<th scope=\"col\">Product Name</th>\n"
             ."<th scope=\"col\">Quantity sold</th>\n"
-            ."<th scope=\"col\">Date sold</th>\n"
             ."</tr>\n";
     // retrieve current record pointed by the result pointer
     
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
-        echo "<td>",$row["sale_id"],"</td>\n";
-        echo "<td>",$row["product_id"],"</td>\n";  
-        echo "<td>",$row["sale_quantity"],"</td>\n";
         echo "<td>",$row["sale_date"],"</td>\n";
+        echo "<td>",$row["sale_id"],"</td>\n";
+        echo "<td>",$row["product_id"],"</td>\n";
+        echo "<td>",$row["product_name"],"</td>\n";    
+        echo "<td>",$row["sale_quantity"],"</td>\n";
         echo "</tr>";
     }
     echo "</table>\n";
-    mysqli_free_result($result);
+    $result->free();
 }
 
 $conn->close();
