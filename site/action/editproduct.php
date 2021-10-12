c5bb67096458402440eb3109c5bbcf7b7a46e112
Elliot Hillary <103074503@student.swin.edu.au>
2021-10-12 22:18:39 +1100

    Edit product commenting

--- site/action/editproduct.php
+++ site/action/editproduct.php
@@ -6,6 +6,8 @@ function fail($msg) {
     echo $msg;
     die();
 }
+
+// make sure all the required fields exist
 if(!isset($_POST["product_id"]))
     fail("Please provide a product ID.");
 if(!isset($_POST["product_name"]))
@@ -14,9 +16,9 @@ if(!isset($_POST["product_description"]))
     fail("Please provide a product description.");
 if(!isset($_POST["product_quantity"]))
     fail("Please provide a product quantity.");
-    
-$id = $_POST["product_id"];
 
+// Check if the product ID already exists or not - it needs to
+$id = $_POST["product_id"];
 $id_exists_stmt = $conn->prepare("SELECT count(*) AS count FROM product WHERE product_id = ?");
 $id_exists_stmt->bind_param("i", $id);
 
@@ -27,19 +29,24 @@ if(!$exists_ok) {
 } else {
     $id_exists_stmt->bind_result($count);
     $id_exists_stmt->fetch();
-
+    
+    // if there were no products with that product ID, fail - we're editing, not creating
     if($count == 0) {
         fail("No product exists with that ID.");
     }
 }
 
+// close ID exists statements
 $id_exists_stmt->close();
 
+// prepare the update statement - see salesreport.php for link on prepared statements
 $update_stmt = $conn->prepare("UPDATE product SET product_name = ?, product_description = ?, product_quantity = ? WHERE product_id = ?");
+// bind the params
 $update_stmt->bind_param("ssii",$_POST["product_name"], $_POST["product_description"], $_POST["product_quantity"], $id);
-
+// execute the statement
 $update_ok = $update_stmt->execute();
 
+// if the update failed, let the user know, otherwise direct to show.php
 if(!$update_ok) {
     fail("Please provide valid values.");
 } else {
