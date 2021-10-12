34cbd12f71d0bad7826e216ac6aa0cb837a58207
ryandonald20 <donaldrj20@gmail.com>
2021-10-13 09:32:50 +1100

    Create editproduct.php

---
+++ site/editproduct.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include 'commits/header.inc'; ?>
+ <title>Edit Product</title>
+</head>
+<body>
+<?php include 'commits/menu.inc';?>
+<h2>Edit Product </h2>
+<section class ="product">
+<form action = "action/editproduct.php" method = "post" >
+	<fieldset>
+		<legend>Details</legend>
+		<p><label for="product_id">Product ID: </label> 
+			<input type="text" id="product_id" name= "product_id" placeholder="0001" required="required"/>
+		</p>
+		<p><label for="product_name">Product name: </label> 
+			<input type="text" id="product_name" name= "product_name" required="required" />
+		</p>
+			
+        <p><label class="product_description">Description: </label></p>  
+			<textarea class="product_description" id="product_description" name="product_description" rows="5" cols="25" placeholder="Enter description of product" required="required"></textarea>
+
+		<p><label for="product_quantity">Product Quantity: </label> 
+			<input type="number" id="product_quantity" name= "product_quantity" required="required" />
+		</p>			
+	</fieldset>
+
+	<input type = "submit" name = "posted" value = "Post">
+	<input type = "reset" value = "Reset">
+</form>
+<br>
+<br>
+<?php
+echo "<a href='index.php'>Return Home</a>";
+?>
+
+</form>
+</section> 
+<?php include 'commits/footer.inc'; ?>
+</body>
+</html>
